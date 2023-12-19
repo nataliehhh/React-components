405ae6021c183dd9d206f67783db59888d1da3be
natalie <hardman.natalie85@gmail.com>
2023-12-19 11:46:52 +0000

    updated client app

--- mini-stack/client/app.js
+++ mini-stack/client/app.js
@@ -2,7 +2,7 @@ async function getMessage() {
     const response = await fetch("http://localhost:8080/message")
     const json = await response.json()
     const p = document.getElementById("message")
-    p.textContent = json
+    p.textContent = json.message
 }
 
 getMessage()
